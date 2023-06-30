874dbe294a0cc80784dd8376124b877b7dc109c1
faliNali <justinle596@gmail.com>
2023-06-29 23:33:06 -0700

    added sliders and pi radians display

--- main.lua
+++ main.lua
@@ -11,6 +11,7 @@ local smallCircleRadius
 local radius
 local radians
 local radianSpeed
+local radianSpeedDef
 
 local sin, cos
 
@@ -20,12 +21,25 @@ local sinPointSpeed
 local sinTimer
 local sinTimerMax
 
+local sliders
+
+local pi_image
+local piOffset 
+
 function math.round(num, place)
 	place = place or 1
 	local factor = 10^place
 	local numx = num * factor
 	local decimal = numx - math.floor(numx)
 	return (decimal < 0.5 and math.floor(numx) or math.ceil(numx)) / factor
+end
+
+function math.clamp(num, min, max)
+	return num < min and min or num > max and max or num
+end
+
+function InRect(x, y, rx, ry, rw, rh)
+	return x >= rx and x <= rx+rw and y >= ry and y <= ry+rh	
 end
 
 function love.load()    
@@ -43,13 +57,14 @@ function love.load()
 	canvas:setFilter('nearest', 'nearest')
 
 	circleOffset = { x = cWidth/2 - 30,
-				     y = cHeight/2 }
+				     y = cHeight/2  - 15}
 
 	smallCircleRadius = 4
 
     radius = 30
 	radians = 0
 	radianSpeed = 2
+	radianSpeedDef = radianSpeed
 
 	sin, cos = math.sin(radians), math.cos(radians)
 
@@ -57,15 +72,87 @@ function love.load()
 	sinPointsUnpacked = {}
 	sinPointSpeed = 35
 	sinTimer = 0
-	sinTimerMax = 0.1
+	sinTimerMax = 0.05
+
+	local sx, sy = 20, 125
+	local sxDiff = 70
+
+	sliders = {}
+	sliders.radians = Slider:new(sx, sy, 0, math.pi*2)
+	sliders.radianSpeed = Slider:new(sx + sxDiff, sy, 0, 6)
+	sliders.radianSpeed:setValue(radianSpeed)
+
+	pi_image = love.graphics.newImage('pi.png')
+	piOffset = 1.6
 end
 
+local function setCanvas()
+    love.graphics.setCanvas(canvas)
+	 	love.graphics.clear()
+
+	---- draw gui here
+	 	love.graphics.print('sin ' .. tostring(math.round(radians, 1)) .. ': ', 5, 5)
+		love.graphics.print(tostring(math.round(sin, 2)), 40, 5)
+		sliders.radians:draw(tostring('rad: ' .. math.round(radians)))	
+		sliders.radianSpeed:draw('speed: ' .. tostring(math.round(radianSpeed)))
+		
+
+	---- draw parts of circle here
+		love.graphics.translate(circleOffset.x, circleOffset.y)
+		-- circle
+		love.graphics.setColor(0.5, 0.5, 0.5)
+		love.graphics.circle('line', 0, 0, radius)
+		
+		-- sin and cos
+		love.graphics.setColor(0.5, 0.5, 1)
+		love.graphics.circle('fill', cos * radius, sin * radius, smallCircleRadius)
+		-- sin
+		love.graphics.setColor(1, 0.8, 0.5)
+		love.graphics.circle('fill', 0, sin * radius, smallCircleRadius)
+
+		--pi 
+		if sliders.radians:isMouseDown() then
+			love.graphics.setColor(1, 1, 1)	
+			local iw, ih = pi_image:getWidth(), pi_image:getHeight()
+			love.graphics.draw(pi_image, cos * radius * piOffset - 6, sin * radius * piOffset, 0, 1, 1, iw/2, ih/2)
+			love.graphics.print(tostring(math.round(radians/math.pi)), cos * radius * piOffset + 6, sin * radius * piOffset, 0, 1, 1, iw/2, ih/2 + 5)
+		end
+
+		-- sin points
+		--[[for i, s in ipairs(sinPoints) do
+			love.graphics.circle('fill', circleOffset.x + s.x * radius, circleOffset.y + s.y * radius, 1)
+		end]]
+
+		-- sin lines
+		love.graphics.setColor(1, 1, 1)
+		if #sinPointsUnpacked >= 4 then love.graphics.line(unpack(sinPointsUnpacked)) end
+
+
+		-- line connecting sin&cos and sin
+		love.graphics.line(cos * radius, sin * radius, 0, sin * radius)
+
+	love.graphics.setCanvas()
+end
 
 function love.update(dt)
+	if sliders.radians:isMouseDown() then
+		radians = sliders.radians:getValue()
+		radianSpeed = 0
+	else
+		radianSpeed = radianSpeedDef
+	end
+
+	radianSpeedDef = sliders.radianSpeed:getValue()
 
 	radians = radians + dt*radianSpeed
+	sliders.radians:setValue(radians)
+
 	if radians >= math.pi*2 then radians = radians - math.pi*2 end
-	
+	local mousex, mousey = love.mouse.getPosition()
+
+	for i, s in pairs(sliders) do
+		s:update(mousex/canvasScale, mousey/canvasScale)
+	end
 	sin, cos = math.sin(radians), math.cos(radians)
 
 	sinTimer = sinTimer + dt
@@ -87,38 +174,14 @@ function love.update(dt)
 	table.insert(sinPointsUnpacked, 0)
 	table.insert(sinPointsUnpacked, sin * radius)
 
-	love.graphics.setCanvas(canvas)
-	 	love.graphics.clear()
-	---- draw gui here
-		love.graphics.print("Radians:\n" .. tostring(math.round(radians)) .. '\npi ' .. tostring(math.round(radians/math.pi)), 10, 10)
-
-	---- draw parts of circle here
-		love.graphics.translate(circleOffset.x, circleOffset.y)
-		-- circle
-		love.graphics.setColor(0.5, 0.5, 0.5)
-		love.graphics.circle('line', 0, 0, radius)
-		
-		-- sin and cos
-		love.graphics.setColor(0.5, 0.5, 1)
-		love.graphics.circle('fill', cos * radius, sin * radius, smallCircleRadius)
-		-- sin
-		love.graphics.setColor(1, 0.8, 0.5)
-		love.graphics.circle('fill', 0, sin * radius, smallCircleRadius)
-
-		-- sin points
-		--[[for i, s in ipairs(sinPoints) do
-			love.graphics.circle('fill', circleOffset.x + s.x * radius, circleOffset.y + s.y * radius, 1)
-		end]]
-
-		-- sin lines
-		love.graphics.setColor(1, 1, 1)
-		if #sinPointsUnpacked >= 4 then love.graphics.line(unpack(sinPointsUnpacked)) end
-
-
-		-- line connecting sin&cos and sin
-		love.graphics.line(cos * radius, sin * radius, 0, sin * radius)
-	love.graphics.setCanvas()
+	setCanvas()
 end	
+
+function love.mousepressed(x, y, button)
+	for i, s in pairs(sliders) do
+		s:mousepressed(x/canvasScale, y/canvasScale, button)
+	end
+end
 
 function love.keypressed(key)
 	if key == 'escape' or 'q' then love.event.quit() end
--- slider.lua
+++ slider.lua
@@ -1,18 +1,66 @@
 local Slider = {}
 Slider.__index = Slider
 
-function Slider:new(x, y)
+function Slider:new(x, y, min, max)
     local s = {}
     setmetatable(s, self)
 
     s.pos = {x = x, y = y}
-    s.test = 'hello'
+    s.rect = {w = 60, h = 12}
+    s.handleWidth = 10 
+
+    s.value = 0.5
+    s.range = {min = min, max = max}
+
+    s.mouseDown = false
+    s.mouseJustPressed = false
 
     return s
 end
 
-function Slider:draw()
-    love.graphics.rectangle('fill', self.pos.x, self.pos.y, 100, 10)
+function Slider:getHandleDimensions()
+    return self.pos.x + self.value*self.rect.w - self.handleWidth/2, self.pos.y, self.handleWidth, self.rect.h
+end
+
+function Slider:setValue(newValue)
+    self.value = (newValue - self.range.min) / (self.range.max - self.range.min)
+end
+
+function Slider:getValue()
+    return self.value * (self.range.max - self.range.min)
+end
+
+function Slider:isMouseDown() return self.mouseDown end
+
+function Slider:inRect(x, y)
+    return InRect(x, y, self.pos.x, self.pos.y, self.rect.w, self.rect.h)
+end
+function Slider:mousepressed(x, y, button)
+    if button == 1 and self:inRect(x, y) then
+        self.mouseJustPressed = true
+        print('wow')
+    end
+end
+
+function Slider:update(mousex, mousey)
+    if (self:inRect(mousex, mousey) or self.mouseDown) and self.mouseJustPressed and love.mouse.isDown(1) then
+        self.mouseDown = true
+        self.value = math.clamp((mousex - self.pos.x)/self.rect.w, 0, 1)
+    else
+        self.mouseDown = false
+        self.mouseJustPressed = false
+    end
+end
+
+function Slider:draw(text)
+    love.graphics.setColor(0.25, 0.25, 0.25)
+    love.graphics.rectangle('fill', self.pos.x, self.pos.y, self.rect.w, self.rect.h)
+
+    love.graphics.setColor(1, 1, 1)
+    local x, y, w, h = self:getHandleDimensions()
+    love.graphics.rectangle('fill', x, y, w, h)
+
+    love.graphics.print(text, self.pos.x, self.pos.y - 14)
 end
 
 return Slider
